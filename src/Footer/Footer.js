a9a7617eec5472eeed211ff189bcbe375c524efa
AmanW-GitHub <amanwaghchoure2004@gmail.com>
2025-02-18 10:03:14 -0500

    Brought Personal Budget to React

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bottom">
+      <div className="center">All rights reserved &copy; Fabio Nolasco</div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <main className="center" id="main" role="main">
+      <section className="page-area">
+        <article>
+          <h2>Stay on track</h2>
+          <p>
+            Do you know where you are spending your money? If you really stop to
+            track it down, you would get surprised! Proper budget management
+            depends on real data... and this app will help you with that!
+          </p>
+        </article>
+
+        <article>
+          <h2>Alerts</h2>
+          <p>
+            What if your clothing budget ended? You will get an alert. The goal
+            is to never go over the budget.
+          </p>
+        </article>
+
+        <article>
+          <h2>Results</h2>
+          <p>
+            People who stick to a financial plan, budgeting every expense, get
+            out of debt faster! Also, they to live happier lives... since they
+            expend without guilt or fear... because they know it is all good and
+            accounted for.
+          </p>
+        </article>
+
+        <article>
+          <h2>Free</h2>
+          <p>This app is free!!! And you are the only one holding your data!</p>
+        </article>
+
+        <article>
+          <h2>Stay on track</h2>
+          <p>
+            Do you know where you are spending your money? If you really stop to
+            track it down, you would get surprised! Proper budget management
+            depends on real data... and this app will help you with that!
+          </p>
+        </article>
+
+        <article>
+          <h2>Alerts</h2>
+          <p>
+            What if your clothing budget ended? You will get an alert. The goal
+            is to never go over the budget.
+          </p>
+        </article>
+
+        <article>
+          <h2>Results</h2>
+          <p>
+            People who stick to a financial plan, budgeting every expense, get
+            out of debt faster! Also, they to live happier lives... since they
+            expend without guilt or fear... because they know it is all good and
+            accounted for.
+          </p>
+        </article>
+
+        <article>
+          <h2>Chart</h2>
+          <p>
+            <canvas id="myChart" width="400" height="400"></canvas>
+          </p>
+        </article>
+        <article>
+          <h2>D3 Chart</h2>
+          <div id="d3chart"></div>
+        </article>
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
